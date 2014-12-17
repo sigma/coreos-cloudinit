3c57252543d0374f84941454c7a1af411f100a79
Yann Hodique <hodiquey@vmware.com>
2014-12-17 10:35:19 -0800

    attempt at reading cloud-init from VMware platform

---
+++ Godeps/_workspace/src/github.com/sigma/vmw-guestinfo/rpcout/rpcout.go
@@ -0,0 +1,99 @@
+package rpcout
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+
+	"github.com/coreos/coreos-cloudinit/Godeps/_workspace/src/github.com/golang/glog"
+	"github.com/coreos/coreos-cloudinit/Godeps/_workspace/src/github.com/sigma/vmw-guestinfo/message"
+)
+
+const RPCI_PROTOCOL_NUM uint32 = 0x49435052
+
+func SendOne(format string, a ...interface{}) ([]byte, error) {
+	request := fmt.Sprintf(format, a...)
+	return SendOneRaw([]byte(request))
+}
+
+func SendOneRaw(request []byte) ([]byte, error) {
+	var reply []byte
+	glog.Infof("Rpci: Sending request='%s'", request)
+
+	status := false
+
+	out := &RpcOut{}
+	err := out.Start()
+	if err != nil {
+		reply = []byte("RpcOut: Unable to open the communication channel")
+	} else {
+		reply, err = out.Send(request)
+		if err != nil {
+			status = true
+		}
+	}
+
+	glog.Infof("Rpci: Sent request='%s', reply='%s', status=%t",
+		request, reply, status)
+
+	err = out.Stop()
+	if err != nil {
+		glog.Infof("Rpci: unable to close the communication channel")
+	}
+
+	return reply, err
+}
+
+type RpcOut struct {
+	channel message.Channel
+}
+
+func (out *RpcOut) Start() error {
+	channel, err := message.Open(RPCI_PROTOCOL_NUM)
+	if err != nil {
+		return errors.New("Could not open channel with RPCI protocol")
+	}
+	out.channel = channel
+	return nil
+}
+
+func (out *RpcOut) Stop() error {
+	err := message.Close(out.channel)
+	out.channel = nil
+	return err
+}
+
+func (out *RpcOut) Send(request []byte) ([]byte, error) {
+	resp := make([]byte, 0)
+	err := message.Send(out.channel, request)
+	if err != nil {
+		return resp, err
+	}
+
+	reply, err := message.Receive(out.channel)
+	if err != nil {
+		return resp, err
+	}
+
+	valid := true
+	if len(reply) < 2 {
+		valid = false
+	} else {
+		resp = reply
+		prefix := resp[:2]
+
+		if bytes.Equal(prefix, []byte("1 ")) {
+			resp = resp[2:]
+		} else if bytes.Equal(prefix, []byte("0 ")) {
+			resp = make([]byte, 0)
+		} else {
+			valid = false
+		}
+	}
+
+	if valid {
+		return resp, nil
+	} else {
+		return nil, errors.New("Invalid format for the result of the RPCI command")
+	}
+}
---
+++ datasource/vmw/guestinfo.go
@@ -0,0 +1,113 @@
+package vmw
+
+import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/coreos/coreos-cloudinit/Godeps/_workspace/src/github.com/sigma/vmw-guestinfo/rpcvmx"
+	"github.com/coreos/coreos-cloudinit/Godeps/_workspace/src/github.com/sigma/vmw-guestinfo/vmcheck"
+	"github.com/coreos/coreos-cloudinit/Godeps/_workspace/src/github.com/sigma/vmw-ovflib"
+)
+
+type guestInfo struct {
+	user_data []byte
+	meta_data []byte
+}
+
+func readVariable(var_name string, ovf_env *ovf.OvfEnvironment) (string, bool) {
+	if val, ok := ovf_env.Properties["guestinfo."+var_name]; ok {
+		return val, ok
+	} else if vmcheck.IsVirtualWorld() {
+		val := rpcvmx.ConfigGetString(var_name, "")
+		return val, val != ""
+	}
+	return "", false
+}
+
+func readUrlBody(url string) []byte {
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	return body
+}
+
+func NewDatasource(filename string) *guestInfo {
+	var ovf_env []byte
+	gi := &guestInfo{}
+
+	if filename == "" {
+		if vmcheck.IsVirtualWorld() {
+			ovf_env = []byte(rpcvmx.ConfigGetString("ovfenv", ""))
+		} else {
+			ovf_env = make([]byte, 0)
+		}
+	} else {
+		var err error
+		ovf_env, err = ioutil.ReadFile(filename)
+		if err != nil {
+			ovf_env = make([]byte, 0)
+		}
+	}
+
+	env := &ovf.OvfEnvironment{}
+	if len(ovf_env) != 0 {
+		env = ovf.ReadEnvironment(ovf_env)
+	}
+
+	val, ok := readVariable("user_data.doc", env)
+	if ok {
+		gi.user_data = []byte(val)
+	} else if val, ok = readVariable("user_data.url", env); ok {
+		gi.user_data = readUrlBody(val)
+	}
+
+	val, ok = readVariable("meta_data.doc", env)
+	if ok {
+		gi.user_data = []byte(val)
+	} else if val, ok = readVariable("meta_data.url", env); ok {
+		gi.meta_data = readUrlBody(val)
+	}
+
+	return gi
+}
+
+func (gi *guestInfo) IsAvailable() bool {
+	return len(gi.user_data) != 0 || len(gi.meta_data) != 0
+}
+
+func (gi *guestInfo) AvailabilityChanges() bool {
+	return false
+}
+
+func (gi *guestInfo) ConfigRoot() string {
+	return ""
+}
+
+func (gi *guestInfo) FetchMetadata() ([]byte, error) {
+	var err error
+	if len(gi.meta_data) == 0 {
+		err = errors.New("No metadata")
+	} else {
+		err = nil
+	}
+	return gi.meta_data, err
+}
+
+func (gi *guestInfo) FetchUserdata() ([]byte, error) {
+	var err error
+	if len(gi.user_data) == 0 {
+		err = errors.New("No metadata")
+	} else {
+		err = nil
+	}
+	return gi.user_data, err
+}
+
+func (gi *guestInfo) FetchNetworkConfig(filename string) ([]byte, error) {
+	return nil, nil
+}
+
+func (gi *guestInfo) Type() string {
+	return "vmw-guestinfo"
+}
